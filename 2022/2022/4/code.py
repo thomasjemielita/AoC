3d468941b76adc39dae644b0dc3375643d0506d6
thomasjemielita <thomasjemielita@gmail.com>
2022-12-04 21:52:51 -0500

    day4_part2

--- 2022/2022/4/code.py
+++ 2022/2022/4/code.py
@@ -4,6 +4,10 @@
 
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
     input = input_file.read()
+
+# Modules #    
+import time
+import pandas as pd
     
 # Start timer #
 print("Starting Timer)
@@ -20,15 +24,20 @@ df['elf2_max'] = df['elf2'].str.split('-').str[1]
 elf_vals = ['elf1_min', 'elf1_max', 'elf2_min', 'elf2_max']
 df[elf_vals] = df[elf_vals].apply(pd.to_numeric)
 
-# Check if elfs overlap (1 within 2, and 2 within 1) #
-df['check_1_2'] = (df['elf2_min'] <= df['elf1_min']) & (df['elf1_min'] <= df['elf2_max']) \
-                   & (df['elf2_min'] <= df['elf1_max'] )& (df['elf1_max'] <= df['elf2_max']) 
-                   
-df['check_2_1'] = (df['elf1_min'] <= df['elf2_min']) & (df['elf2_min'] <= df['elf1_max']) \
-                   & (df['elf1_min'] <= df['elf2_max'] )& (df['elf2_max'] <= df['elf1_max']) 
+# Part 1:: Check if elfs overlap (1 within 2, and 2 within 1) #
+df['check_1_2_min'] = (df['elf2_min'] <= df['elf1_min']) & (df['elf1_min'] <= df['elf2_max'])
+df['check_1_2_max'] = (df['elf2_min'] <= df['elf1_max'] )& (df['elf1_max'] <= df['elf2_max']) 
+df['check_2_1_min'] = (df['elf1_min'] <= df['elf2_min']) & (df['elf2_min'] <= df['elf1_max'])
+df['check_2_1_max'] = (df['elf1_min'] <= df['elf2_max'] )& (df['elf2_max'] <= df['elf1_max']) 
 
 # Select instances where one or both elfs overlap #
-num_overlapping_pairs = df.loc[df['check_1_2'] | df['check_2_1']].shape[0]
+num_part1_pairs = df.loc[(df['check_1_2_min'] & df['check_1_2_max']) | (df['check_2_1_min'] & df['check_2_1_max'])].shape[0]
 
-stop_time = time.time() - start_time
-print("Total ({time} s): {value}".format(time = round(stop_time,3), value = str(num_overlapping_pairs)))
+part1_time = time.time() - start_time
+print("Total ({time} s): {value}".format(time = round(part1_time,3), value = str(num_part1_pairs)))
+
+# Part 2: Look for any overlap
+num_part2_pairs = df.loc[(df['check_1_2_min'] | df['check_1_2_max']) | (df['check_2_1_min'] | df['check_2_1_max'])].shape[0]
+
+part2_time = time.time() - start_time
+print("Total ({time} s): {value}".format(time = round(part2_time,3), value = str(num_part2_pairs)))
